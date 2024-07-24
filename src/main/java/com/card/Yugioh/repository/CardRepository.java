30fbec06bda5e543bbb2ba311ba2c8e47d99aaa1
asbazq <wjdgns5488@naver.com>
2024-07-25 00:29:05 +0900

    pref : 검색 기능 수정

--- src/main/java/com/card/Yugioh/repository/CardRepository.java
+++ src/main/java/com/card/Yugioh/repository/CardRepository.java
@@ -6,9 +6,18 @@ import org.springframework.data.repository.query.Param;
 
 import com.card.Yugioh.model.CardModel;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface CardRepository extends JpaRepository<CardModel, Long> {
-    @Query("SELECT c FROM CardModel c WHERE LOWER(c.name) LIKE LOWER(CONCAT ('%', :query, '%'))")
-    List<CardModel> searchByNameContaining(@Param("query")String query);
+    // @Query("SELECT c FROM CardModel c WHERE LOWER(c.name) LIKE LOWER(CONCAT ('%', :query, '%'))")
+    // List<CardModel> searchByNameContaining(@Param("query")String query);
+
+    // name 대,문자 구분 없이 검색 및 띄어쓰기 무시
+    @Query("SELECT c FROM CardModel c WHERE LOWER(REPLACE(c.korName, ' ', '')) LIKE LOWER(REPLACE(CONCAT('%', :query, '%'), ' ', '')) OR LOWER(REPLACE(c.name, ' ', '')) LIKE LOWER(REPLACE(CONCAT('%', :query, '%'), ' ', ''))")
+    List<CardModel> searchByNameContaining(@Param("query") String query);
+
+    
+    CardModel findByName(String name);
+    Optional<CardModel> findByKorName(String korName);
 }
