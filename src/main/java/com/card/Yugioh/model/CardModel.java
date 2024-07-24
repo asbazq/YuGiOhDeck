227597bb942ad4bae798ef676da5dc74e6862985
asbazq <wjdgns5488@naver.com>
2024-07-25 00:28:31 +0900

    feat : 정보 추가

--- src/main/java/com/card/Yugioh/model/CardModel.java
+++ src/main/java/com/card/Yugioh/model/CardModel.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
@@ -18,9 +19,13 @@ public class CardModel {
     @Id
     private Long id;
     private String name;
+    private String korName;
     private String type;
     private String frameType;
-    // private String desc;
+    @Column(length = 1000)
+    private String desc;
+    @Column(length = 1000)
+    private String korDesc;
     private int atk;
     private int def;
     private int level;
